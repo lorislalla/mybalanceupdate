76bc567521ec0dc13c1241b1a4a9acae96893493
Loris <loris.lalla@evolu-s.it>
2025-12-16 11:27:30 +0100

    Aggiunto service worker per webapp

--- index.tsx
+++ index.tsx
@@ -1,10 +1,11 @@
 
 import { bootstrapApplication } from '@angular/platform-browser';
-import { provideZonelessChangeDetection, LOCALE_ID } from '@angular/core';
+import { provideZonelessChangeDetection, LOCALE_ID, isDevMode } from '@angular/core';
 import { AppComponent } from './src/app.component';
 import { provideHttpClient } from '@angular/common/http';
 import { registerLocaleData } from '@angular/common';
 import localeIt from '@angular/common/locales/it';
+import { provideServiceWorker } from '@angular/service-worker';
 
 registerLocaleData(localeIt, 'it-IT');
 
@@ -12,7 +13,10 @@ bootstrapApplication(AppComponent, {
   providers: [
     provideZonelessChangeDetection(),
     provideHttpClient(),
-    { provide: LOCALE_ID, useValue: 'it-IT' }
+    { provide: LOCALE_ID, useValue: 'it-IT' }, provideServiceWorker('ngsw-worker.js', {
+            enabled: !isDevMode(),
+            registrationStrategy: 'registerWhenStable:30000'
+          })
   ]
 }).catch(err => console.error(err));
 
